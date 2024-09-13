522f73bf65a0bbb61a2af65acab5bb73bcedea32
Leonel Otavalo <leoandy28aa@gmail.com>
2024-09-13 17:41:43 -0500

    Add payments microservice

---
+++ src/payments/payments.service.ts
@@ -0,0 +1,71 @@
+import { Injectable } from '@nestjs/common';
+import { envs } from 'src/config';
+import Stripe from 'stripe';
+import { Request, Response } from 'express';
+import { PaymentSessionDto } from './dto/payment-session.dto';
+
+@Injectable()
+export class PaymentsService {
+  private readonly stripe = new Stripe(envs.STRIPE_SECRET);
+
+  async createPaymentSession(paymentSessionDto: PaymentSessionDto) {
+    const { currency, items, orderId } = paymentSessionDto;
+    const lineItems = items.map((item) => {
+      return {
+        price_data: {
+          currency: currency,
+          product_data: {
+            name: item.name,
+          },
+          unit_amount: Math.round(item.price * 100), //This means $20.00
+        },
+        quantity: item.quantity,
+      };
+    });
+
+    const session = await this.stripe.checkout.sessions.create({
+      payment_intent_data: {
+        metadata: {
+          orderId,
+        },
+      },
+      line_items: lineItems,
+      mode: 'payment',
+      success_url: envs.STRIPE_SUCCESS_URL,
+      cancel_url: envs.STRIPE_CANCEL_URL,
+    });
+    return session;
+  }
+
+  async stripeWebhook(req: Request, res: Response) {
+    const sig = req.headers['stripe-signature'];
+    let event: Stripe.Event;
+    const endpointSecret = envs.STRIPE_ENDPOINT_SECRET;
+
+    try {
+      event = this.stripe.webhooks.constructEvent(
+        req['rawBody'],
+        sig,
+        endpointSecret,
+      );
+    } catch (err) {
+      res.status(400).send(`Webhook Error: ${err.message}`);
+      return;
+    }
+
+    switch (event.type) {
+      case 'charge.succeeded':
+        const chargeSucceeded = event.data.object;
+        // TODO: Call microservice to update order status
+        console.log({
+          metadata: chargeSucceeded.metadata,
+          orderId: chargeSucceeded.metadata.orderId,
+        });
+        break;
+      default:
+        console.log(`Unhandled event type ${event.type}`);
+    }
+
+    return res.status(200).json({ sig });
+  }
+}
